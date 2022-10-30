64866b74e759eefea44885b3645662d2b311da72
VoltChange <83451689+VoltChange@users.noreply.github.com>
2022-10-30 21:53:23 +0800

    sa-token集成redis

--- service/service_demo/src/main/java/com/lvpaul/shiyi/demo/controller/StudentController.java
+++ service/service_demo/src/main/java/com/lvpaul/shiyi/demo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.lvpaul.shiyi.demo.controller;
 
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.lvpaul.shiyi.demo.rpc.RemoteCeshiService;
 import com.lvpaul.shiyi.pojo.entity.demo.Student;
 import com.lvpaul.shiyi.demo.mapper.StudentMapper;
@@ -29,6 +30,16 @@ public class StudentController {
     private StudentMapper studentMapper;
     @Autowired
     private RemoteCeshiService remoteCeshiService;
+    @PostMapping("doLogin/{id}")
+    public Result doLogin(@PathVariable int id){
+        StpUtil.login(id);
+
+        return Result.success(StpUtil.getLoginId());
+    }
+    @GetMapping("testLogin")
+    public Result testLogin(){
+        return Result.success(StpUtil.getLoginId());
+    }
     @GetMapping("findAll")
     public Result findAllStudent(){
         return  Result.success(studentService.list());
