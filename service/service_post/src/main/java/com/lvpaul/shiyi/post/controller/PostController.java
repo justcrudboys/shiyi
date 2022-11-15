06c070eb902ad198f96483e35c07d4cf50ccab1e
strider130084 <1104771778@qq.com>
2022-11-15 16:36:03 +0800

    Update PostController.java

--- service/service_post/src/main/java/com/lvpaul/shiyi/post/controller/PostController.java
+++ service/service_post/src/main/java/com/lvpaul/shiyi/post/controller/PostController.java
@@ -3,12 +3,14 @@ package com.lvpaul.shiyi.post.controller;
 import com.lvpaul.shiyi.post.service.PostService;
 import com.lvpaul.shiyi.pojo.entity.post.Post;
 import com.lvpaul.shiyi.pojo.vo.post.PostRequestVo;
-
 import com.lvpaul.shiyi.utils.result.Result;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
