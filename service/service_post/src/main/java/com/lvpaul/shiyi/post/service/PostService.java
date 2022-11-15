eb47d914e47f0a718003881655f60d4132ca79ec
strider130084 <108875410+strider130084@users.noreply.github.com>
2022-11-15 16:34:41 +0800

    Revert "频道管理部分"

--- service/service_post/src/main/java/com/lvpaul/shiyi/post/service/PostService.java
+++ service/service_post/src/main/java/com/lvpaul/shiyi/post/service/PostService.java
@@ -3,8 +3,5 @@ package com.lvpaul.shiyi.post.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lvpaul.shiyi.pojo.entity.post.Post;
 
-import java.util.List;
-
 public interface PostService extends IService<Post> {
-    public List<Post> getChannelPost(Long channel_id);
 }
