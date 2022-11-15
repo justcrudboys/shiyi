a218017eea014410724e8e7278bd9725a017461c
strider130084 <108875410+strider130084@users.noreply.github.com>
2022-11-15 18:14:23 +0800

    Revert "Revert "频道管理部分""

--- service/service_post/src/main/java/com/lvpaul/shiyi/post/service/impl/PostServiceImpl.java
+++ service/service_post/src/main/java/com/lvpaul/shiyi/post/service/impl/PostServiceImpl.java
@@ -1,11 +1,26 @@
 package com.lvpaul.shiyi.post.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lvpaul.shiyi.post.service.PostService;
 import com.lvpaul.shiyi.pojo.entity.post.Post;
 import com.lvpaul.shiyi.post.mapper.PostMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements PostService {
+    @Autowired
+    PostService postService;
+    @Autowired
+    PostMapper postMapper;
+
+    @Override
+    public List<Post> getChannelPost(Long channel_id){
+        QueryWrapper<Post> wrapper = new QueryWrapper<>();
+        wrapper.eq("channel_id",channel_id);
+        List<Post> postList = postMapper.selectList(wrapper);
+        return postList;
+    }
 }
