addff886edadd4119fc70f76e60364bf2f495075
VoltChange <prpr23333@163.com>
2022-11-17 00:18:35 +0800

    模块创建

---
+++ service/service_subscription/src/main/java/com/lvpaul/shiyi/subscription/config/MpConfig.java
@@ -0,0 +1,9 @@
+package com.lvpaul.shiyi.subscription.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.lvpaul.shiyi.subscription.mapper")
+public class MpConfig {
+}
