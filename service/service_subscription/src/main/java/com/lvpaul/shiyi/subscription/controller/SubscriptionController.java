1ec47900375a983680fd13bd5f6e61383704ebe1
VoltChange <prpr23333@163.com>
2022-11-17 22:18:09 +0800

    用户订阅功能

--- service/service_subscription/src/main/java/com/lvpaul/shiyi/subscription/controller/SubscriptionController.java
+++ service/service_subscription/src/main/java/com/lvpaul/shiyi/subscription/controller/SubscriptionController.java
@@ -8,6 +8,7 @@ import com.lvpaul.shiyi.utils.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @RestController
@@ -24,16 +25,22 @@ public class SubscriptionController {
     }
     @PostMapping
     public Result subscribe(@RequestBody SubscriptionRequestVo subscriptionRequestVo){
-        System.out.println(subscriptionRequestVo.toString());
+        int month = subscriptionRequestVo.getMonth();
+
         QueryWrapper<Subscription> wrapper = new QueryWrapper<>();
         wrapper.eq("plan_id", subscriptionRequestVo.getPlanId())
                 .eq("user_id", subscriptionRequestVo.getUserId());
         Subscription subscription = subscriptionService.getOne(wrapper);
         if(subscription!=null){
-            //订阅信息非空，更新订阅信息
-            System.out.println(subscription.toString());
-            subscription.setExpireTime(new Date());
-            if(subscriptionService.save(subscription))
+            //订阅信息非空，更新订阅失效时间
+            LocalDateTime newExprieTime;
+            if(subscription.getExpireTime().isAfter(LocalDateTime.now()))//判断失效时间是在今天之前还是之后
+                newExprieTime = subscription.getExpireTime().plusMonths(month);
+            else
+                newExprieTime = LocalDateTime.now().plusMonths(month);
+            subscription.setExpireTime(newExprieTime);
+
+            if(subscriptionService.update(subscription,wrapper))
                 return Result.success();
             else
                 return Result.error();
@@ -42,7 +49,7 @@ public class SubscriptionController {
             Subscription newSubscription = new Subscription();
             newSubscription.setPlanId(subscriptionRequestVo.getPlanId());
             newSubscription.setUserId(subscriptionRequestVo.getUserId());
-            newSubscription.setExpireTime(new Date());
+            newSubscription.setExpireTime(LocalDateTime.now().plusMonths(month));//从今日起增加月份
             if(subscriptionService.save(newSubscription))
                 return Result.success();
             else
