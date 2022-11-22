a87cb887e46ba35a4138415ab105e1238e8eb3ff
VoltChange <prpr23333@163.com>
2022-11-23 00:29:37 +0800

    修复权限

--- service/service_post/src/main/java/com/lvpaul/shiyi/post/service/impl/PostServiceImpl.java
+++ service/service_post/src/main/java/com/lvpaul/shiyi/post/service/impl/PostServiceImpl.java
@@ -2,6 +2,8 @@ package com.lvpaul.shiyi.post.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lvpaul.shiyi.pojo.entity.channel.Channel;
+import com.lvpaul.shiyi.post.mapper.ChannelMapper;
 import com.lvpaul.shiyi.post.service.PostService;
 import com.lvpaul.shiyi.pojo.entity.post.Post;
 import com.lvpaul.shiyi.post.mapper.PostMapper;
@@ -15,6 +17,8 @@ public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements Po
     PostService postService;
     @Autowired
     PostMapper postMapper;
+    @Autowired
+    ChannelMapper channelMapper;
 
     @Override
     public List<Post> getChannelPost(Long channel_id){
@@ -22,5 +26,12 @@ public class PostServiceImpl extends ServiceImpl<PostMapper, Post> implements Po
         wrapper.eq("channel_id",channel_id);
         List<Post> postList = postMapper.selectList(wrapper);
         return postList;
+    }
+
+    @Override
+    public Long getPostHost(Long postId) {
+        Post post = postMapper.selectById(postId);
+        Channel channel = channelMapper.selectById(post.getChannelId());
+        return channel.getCreatorId();
     }
 }
