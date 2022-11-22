6ca739cb60ce99c039f99a196d483e5216598ac5
VoltChange <prpr23333@163.com>
2022-11-22 20:40:16 +0800

    订阅权限和订单联调

--- service/service_order/src/main/java/com/lvpaul/shiyi/order/service/impl/OrderServiceImpl.java
+++ service/service_order/src/main/java/com/lvpaul/shiyi/order/service/impl/OrderServiceImpl.java
@@ -4,7 +4,12 @@ import com.lvpaul.shiyi.order.mapper.OrderMapper;
 import com.lvpaul.shiyi.order.service.OrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lvpaul.shiyi.pojo.entity.order.Order;
+import com.lvpaul.shiyi.pojo.vo.order.OrderDetailVo;
 import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +21,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrderMapper, Order> implements OrderService {
-
+    public List<OrderDetailVo> getDetailList(List<Order> orderList){
+        List<OrderDetailVo> orderDetailVoList = orderList.stream().map(res-> new OrderDetailVo(
+                res.getId(), res.getUserId(), "频道名字", res.getPlanId(),
+                "方案名字",66, res.getSubscribeMonth(),
+                res.getMoneyAmount(), res.getStatus()
+        )).collect(Collectors.toList());
+        return orderDetailVoList;
+    }
 }
