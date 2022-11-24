e32633b5c27704f40396aeabb3058cd71408a05a
V1ct-0R <913606086@qq.com>
2022-11-24 19:54:03 +0800

    搜索创作者信息

--- service/service_creator/src/main/java/com/lvpaul/shiyi/creator/service/UserService.java
+++ service/service_creator/src/main/java/com/lvpaul/shiyi/creator/service/UserService.java
@@ -3,7 +3,9 @@ package com.lvpaul.shiyi.creator.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.lvpaul.shiyi.pojo.entity.user.User;
 
+import java.util.List;
+
 
 public interface UserService extends IService<User> {
-
+    public List<User> getUserByName(String name);
 }
